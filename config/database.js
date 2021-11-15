b54951ac1b563749b6bc33b93d79aea5f2f5174c
Keveluk <carlos.stefano@hotmail.com.br>
2021-11-15 16:16:02 -0300

    initial commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'ns928.hostgator.com.br'),
+        port: env('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'dropsc49_diversitrampos'),
+        username: env('DATABASE_USERNAME', 'dropsc49_admin'),
+        password: env('DATABASE_PASSWORD', 'Igualdade@2021'),
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '850b2530-6cb9-43c8-bf20-51d526c89421'
+};
